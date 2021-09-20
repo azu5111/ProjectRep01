2799769673be2966c6ecae7fb72eb1dfe88233c8
azu5111 <azu5111@163.com>
2021-09-20 11:39:44 +0800

    第一次提交

---
+++ src/main/java/com/javacto/po/GitTest001.java
@@ -0,0 +1,11 @@
+package com.javacto.po;
+
+/**
+ * 作者：刘正
+ */
+public class GitTest001 {
+    public static void main(String[] args) {
+        System.out.println("测试Git");
+        System.out.println("测试");
+    }
+}
